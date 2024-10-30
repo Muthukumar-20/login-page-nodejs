aa526c13c2141b8fc253bec705306c43e9124f45
Muthu kumar <mk2778555@gmail.com>
2024-10-30 19:31:53 +0530

    update

--- server.js
+++ server.js
@@ -6,25 +6,22 @@ import router from "./Routers/router.js";
 
 dotenv.config();
 
-const app =express()
+const app = express();
 
-app.use(express.json())
-
+app.use(express.json());
 
 app.use(cors());
 
 connectDB();
 
-
-app.get("/",(req,res)=>{
-    res.status(200).send("welcom to our api");
+app.get("/", (req, res) => {
+  res.status(200).send("welcom to our api");
 });
 
-app.use("/api/auth",router)
+app.use("/api/auth", router);
 
+const port = process.env.PORT || 4000;
 
-const port=process.env.port || 4000;
-
-app.listen(port,()=>{
-    console.log("server started");
+app.listen(port, () => {
+  console.log("server started");
 });
